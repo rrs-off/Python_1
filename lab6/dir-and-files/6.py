6b8767c49e34538dff04a1ddc4b3f13afece0b51
rrs-off <157564773+rrs-off@users.noreply.github.com>
2024-03-08 13:47:00 +0500

    6.py

--- lab6/dir-and-files/6.py
+++ lab6/dir-and-files/6.py
@@ -1,6 +1,6 @@
 import os
 
-path = r"C:\Users\Erasyl\Desktop\python_1\lab\lab6\folder"
+path = r"C:\Users\Ramazan\Desktop\python_1\lab\lab6\folder"
 
 for i in range(65,91):
     name = os.path.join(path, chr(i) + ".txt")
